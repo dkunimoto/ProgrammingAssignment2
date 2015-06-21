55875513455f6460a592315ae5d6e193ffe26a84
Daniel Kunimoto <dkunimoto@gmail.com>
2015-06-21 09:46:54 -0400

    Third commit

--- cachematrix.R
+++ cachematrix.R
@@ -23,14 +23,13 @@ cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
      inv <- x$getinverse()
      if(!is.null(inv)){
-          print("getting cached data")
-          inv
-          
+          print("getting cached data")  # Retrieves the inverse already cached
+          inv                           # in memory
      }
      else
      {
           data <- x$get()
-          inv <- solve(data)
-          x$setinverse(inv)
+          inv <- solve(data)            # Calculates the inverse and sets it
+          x$setinverse(inv)             # in memory when not already assigned
      }
 }
